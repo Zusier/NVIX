d73a8e1ba2d54a6450e5dcebdffd3dbc2c80db11
Zusier <zusier@disroot.org>
2022-07-16 10:51:25 -0500

    Return multiple links #1

--- src/dl.rs
+++ src/dl.rs
@@ -1,13 +1,14 @@
+use std::error::Error;
+
 ///! All code related to downloading: fetching lists, downloading binaries, etc.
 
-const BASE_LINK: &str = "https://international.download.nvidia.com/Windows/";
+const BASE_LINK: &str = "https://international.download.nvidia.com/Windows";
 
 pub struct Driver {
     pub version: String,
     pub channel: DriverChannels,
     pub platform: DriverPlatform,
     pub edition: DriverEdition,
-    pub winver: DriverWindowsVersion,
 }
 
 #[derive(Default, PartialEq)]
@@ -21,7 +22,7 @@ impl std::fmt::Display for DriverChannels {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             DriverChannels::GameReady => write!(f, ""),
-            DriverChannels::Studio => write!(f, "nsd"),
+            DriverChannels::Studio => write!(f, "-nsd"),
         }
     }
 }
@@ -52,7 +53,7 @@ pub enum DriverEdition {
 impl std::fmt::Display for DriverEdition {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            DriverEdition::DCH => write!(f, "dch"),
+            DriverEdition::DCH => write!(f, "-dch"),
             DriverEdition::STD => write!(f, ""),
         }
     }
@@ -68,33 +69,29 @@ pub enum DriverWindowsVersion {
 impl std::fmt::Display for DriverWindowsVersion {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            DriverWindowsVersion::Win10 => write!(f, "win10"),
-            DriverWindowsVersion::Win11 => write!(f, "win10-win11"),
+            DriverWindowsVersion::Win10 => write!(f, "-win10"),
+            DriverWindowsVersion::Win11 => write!(f, "-win10-win11"),
         }
     }
 }
 
-pub fn new_link(driver: &Driver) -> Result<String, String> {
-    if driver.winver == DriverWindowsVersion::Win11 && driver.edition == DriverEdition::STD {
-        return Err(
-            "Windows 11 is not compatible with a non-dch version! Please use DCH.".to_string(),
-        );
+impl DriverWindowsVersion {
+    pub fn iter() -> std::slice::Iter<'static, Self> {
+        [Self::Win10, Self::Win11].iter()
     }
+}
 
+pub fn new_link(driver: &Driver) -> Result<Vec<String>, Box<dyn Error>> {
     let version: &str = &driver.version;
     let platform: &str = &driver.platform.to_string();
-    let winver: &str = &driver.winver.to_string();
+    let channel: &str = &driver.channel.to_string();
+    let edition: &str = &driver.edition.to_string();
 
     // Construct link with values that always exist
-    let mut link: String =
-        format!("{BASE_LINK}{version}/{version}-{platform}-{winver}-64bit-international");
+    let links: Vec<String> = DriverWindowsVersion::iter().map(|winver| {
+        let link: String = format!("{BASE_LINK}/{version}/{version}-{platform}-{winver}-64bit-international{channel}{edition}-whql.exe");
+        link
+    }).collect();
 
-    if driver.channel == DriverChannels::Studio {
-        link.push_str("-nsd");
-    }
-    if driver.edition == DriverEdition::DCH {
-        link.push_str("-dch");
-    }
-    link.push_str("-whql.exe");
-    Ok(link)
+    Ok(links)
 }
