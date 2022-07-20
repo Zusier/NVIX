06b556e4364ed58840fb18f1fa9bc366aceb83fe
Zusier <zusier@disroot.org>
2022-07-20 17:24:04 -0500

    Start interactive mode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, io::Write};
 
 use clap::Parser;
 mod nvapi;
@@ -16,11 +16,54 @@ struct Args {
 
     #[clap(long, value_parser, default_value = "false")]
     test: bool,
+
+    #[clap(long, short)]
+    interactive: bool,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
+    if args.interactive {
+        interactive_mode().await;
+    }
     Ok(())
+}
+
+
+async fn interactive_mode() {
+    let mut gpu = nvapi::detect_gpu().await;
+    match gpu {
+        Ok(mut gpu) => {
+            println!("Detected GPU: {gpu}");
+            if choice("Is this correct?") {
+                // query.. driver
+            }
+            gpu = tui::gpu_selector().await.unwrap().unwrap().name;
+        }
+        Err(_) => {
+            println!("Detected GPU: Unkonwn");
+            println!("No GPU detected, please specify a GPU manually...");
+            std::thread::sleep(std::time::Duration::from_secs(1));
+            gpu = Ok(tui::gpu_selector().await.unwrap().unwrap().name);
+        }
+    }
+}
+
+/// Prints prompt with a y/n amswer, if it is invalid it will simply clear the prompt and recurse
+fn choice(prompt: &str) -> bool {
+    print!("{} [y/n] ", prompt);
+    let mut input = String::new();
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input = input.trim().to_string();
+    if input.to_lowercase() == "y" {
+        return true;
+    } else if input.to_lowercase() == "n" {
+        return false;
+    }
+    // Invalid, repeat
+    print!("\x1b[1A\x1b[K"); // Clears the line
+    choice(prompt)
 }
