70066d31eb20139074b12cee259009937a9a1a69
Zusier <zusier@disroot.org>
2022-08-07 23:04:37 -0500

    Begin slint gui

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use std::{error::Error, io::Result, io::Write, path::PathBuf};
+use std::{io::Result, io::Write, path::PathBuf};
 
 use clap::Parser;
-use crossterm::style::Stylize;
+use nvapi::{xml::get_gpu_list, detect_gpu};
 use once_cell::sync::Lazy;
+use slint::{SharedString, ModelRc};
 
 use crate::nvapi::{xml::XmlGpuEntry, DriverChannels, DriverEdition, DriverPlatform};
 mod nvapi;
 mod setup;
 #[cfg(test)]
 mod tests;
-#[cfg(feature = "tui")]
-mod tui;
+mod ui;
 
 static TMP_FILE: Lazy<PathBuf> = Lazy::new(|| {
     let mut path = PathBuf::from(std::env::temp_dir());
@@ -27,6 +27,7 @@ static TMP_SEVENZIP_FILE: Lazy<PathBuf> = Lazy::new(|| {
     path.push("tmp_7z.exe");
     path
 });
+slint::include_modules!();
 
 /// A light-weight program to download, strip, tweak, and install a NVIDIA driver
 #[derive(Parser, Debug)]
@@ -48,120 +49,43 @@ struct Args {
 async fn main() -> Result<()> {
     let args = Args::parse();
 
-    interactive_mode().await;
+    let orig: Vec<XmlGpuEntry> = get_gpu_list().await.unwrap();
+    let list: slint::ModelRc<SharedString> = xml_vec_to_slint_vec(&orig.clone(), None);
+
+    let ui = AppWindow::new();
+    let ui_weak = ui.as_weak();
+    let ui_weak_pages = ui_weak.clone();
+    ui.set_list(list);
+    ui.on_search(move |search| {
+        let ui = ui_weak.upgrade().unwrap();
+        ui.set_list(xml_vec_to_slint_vec(&orig, Some(search.clone().as_str())));
+    });
+    ui.on_page_move(move |page| {
+        let ui = ui_weak_pages.upgrade().unwrap();
+        println!("On page: {}", page);
+        match page {
+            1 => {
+                println!("{}", ui.get_selection());
+            }
+            _ => {} 
+        }
+    });
+    ui.run();
     Ok(())
 }
 
-async fn interactive_mode() {
-    #[cfg(target_os = "windows")]
-    let gpu = nvapi::detect_gpu().await;
-    #[cfg(target_os = "windows")]
-    let gpu: XmlGpuEntry = match gpu {
-        Ok(gpu) => {
-            println!("Detected GPU: {}", gpu.clone().green());
-            if choice("Is this correct?") {
-                let list = nvapi::xml::get_gpu_list().await;
-                let gpu: XmlGpuEntry = list
-                    .unwrap()
-                    .iter()
-                    .find(|g| g.name == gpu)
-                    .unwrap()
-                    .clone();
-                gpu
-            } else {
-                tui::gpu_selector()
-                    .await
-                    .unwrap()
-                    .expect("GPU not selected, ui closed.")
+/// I fucking love strong types!
+fn xml_vec_to_slint_vec(xml: &Vec<XmlGpuEntry>, filter: Option<&str>) -> ModelRc<SharedString> {
+        let list: Vec<slint::SharedString> = match filter {
+            Some(filter) => {
+                xml.iter().filter(|x| x.name.contains(&*filter)).map(|x| slint::SharedString::from(&x.name)).collect()
             }
-        }
-        Err(_) => {
-            println!("Detected GPU: {}", "Unknown".red());
-            println!("No GPU detected, please specify a GPU manually...");
-            std::thread::sleep(std::time::Duration::from_secs(2));
-            tui::gpu_selector()
-                .await
-                .unwrap()
-                .expect("GPU not selected, ui closed.")
-        }
-    };
-    #[cfg(not(target_os = "windows"))]
-    let gpu = tui::gpu_selector()
-        .await
-        .unwrap()
-        .expect("GPU not selected, ui closed.");
-    clear();
-    println!("GPU Selected: {}", gpu.name.as_str().green()); // Not sure why adding terminal color requires a borrow but ok.
-    let latest = choice("Use the latest driver or choose manually?");
-    let channel = if choice("Use Game Ready or Studio driver?") {
-        DriverChannels::GameReady
-    } else {
-        DriverChannels::Studio
-    };
-    let platform = if choice("Desktop or Mobile GPU?") {
-        DriverPlatform::Desktop
-    } else {
-        DriverPlatform::Notebook
-    };
-    let edition = if choice("Use DCH (preferred) or Standard?") {
-        DriverEdition::DCH
-    } else {
-        DriverEdition::STD
-    };
-    if latest {
-        let driver = nvapi::Driver {
-            version: "".to_string(),
-            channel,
-            platform,
-            edition,
+            None => {
+                xml.iter().map(|x| slint::SharedString::from(&x.name)).collect()
+            }
         };
-        nvapi::download(nvapi::get_latest_driver_link(gpu, driver).await.unwrap())
-            .await
-            .unwrap();
-    } else {
-        println!("Enter your driver version: ");
-        let mut input = String::new();
-        std::io::stdout().flush().unwrap();
-        std::io::stdin().read_line(&mut input).unwrap();
-        input = input.trim().to_string();
-        let driver = nvapi::Driver {
-            version: input,
-            channel,
-            platform,
-            edition,
-        };
-        nvapi::download(nvapi::new_link(&driver).await.unwrap()[0].clone())
-            .await
-            .unwrap(); // TODO: if no valid links exist.. use latest driver as fallback.
-    }
-    nvapi::extract().await.expect("Well shit");
-}
+        let list: slint::VecModel<slint::SharedString> = list.into();
+        let list: slint::ModelRc<slint::SharedString> = slint::ModelRc::new(list);
 
-/// Prints prompt with a y/n amswer, if it is invalid it will simply clear the prompt and recurse
-fn choice(prompt: &str) -> bool {
-    print!("{} [y/n] ", prompt);
-    let mut input = String::new();
-    std::io::stdout().flush().unwrap();
-    std::io::stdin().read_line(&mut input).unwrap();
-    input = input.trim().to_string();
-    if input.to_lowercase() == "y" {
-        return true;
-    } else if input.to_lowercase() == "n" {
-        return false;
-    }
-    // Invalid, repeat
-    clear_ln();
-    choice(prompt)
-}
-
-#[inline(always)]
-/// Clears the terminal and resets cursor to top left
-fn clear() {
-    print!("\x1B[2J\x1B[1;1H");
-}
-
-#[inline(always)]
-/// Clears the current line
-fn clear_ln() {
-    print!("\x1b[1A\x1b[K");
+        list
 }
